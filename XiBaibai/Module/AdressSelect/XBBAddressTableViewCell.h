7e1df57f0513d73c289a093ea8d38cd88992bd5b
laiquanlong1 <hotia@hotiademacbook-pro.local>
2015-12-10 21:38:48 +0800

    1.dizhi

---
+++ XiBaibai/Module/AdressSelect/XBBAddressTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  XBBAddressTableViewCell.h
+//  XiBaibai
+//
+//  Created by HoTia on 15/12/9.
+//  Copyright © 2015年 Mingle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XBBAddressTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *headImageView;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *labelHeadView;
+@property (weak, nonatomic) IBOutlet UILabel *labelLabel;
+@end
---
+++ XiBaibai/Module/AdressSelect/XBBAddressTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  XBBAddressTableViewCell.m
+//  XiBaibai
+//
+//  Created by HoTia on 15/12/9.
+//  Copyright © 2015年 Mingle. All rights reserved.
+//
+
+#import "XBBAddressTableViewCell.h"
+
+@implementation XBBAddressTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
